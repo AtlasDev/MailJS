0de40c358a3f4b0719ecff264e1ba2fe35ebbc5b
Dany Sluijk <danysluyk@live.nl>
2015-06-04 15:09:25 +0200

    moved mails to root scope

--- plugins/web/public/dist/js/controllers.js
+++ plugins/web/public/dist/js/controllers.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller("MailCtrl", function($scope, $window, $translate, socket) {
+app.controller("MailCtrl", function($rootScope, $scope, $window, $translate, socket) {
     $scope.socketStatus = 0;
     $scope.user = {}
     $scope.user.firstName = "Dany";
@@ -37,49 +37,77 @@ app.controller("MailCtrl", function($scope, $window, $translate, socket) {
     socket.on('reconnecting', function (error) {
         $scope.socketStatus = 1;
     });
-});
-
-app.controller('mailboxController', function($scope, socket) {
-	$scope.title = 'Mailbox';
-	$scope.subtitle = '';
-	$scope.page = 1;
-	$scope.limit = 15;
-    $scope.toggleStar = function star(uuid) {
-        for(var i in $scope.mails) {
-            if($scope.mails[i].uuid == uuid) {
-                socket.emit('mail:star', {uuid: uuid, state: !$scope.mails[i].starred});
-                break;
-            }
-        }
-    }
+	
+	$rootScope.mails = [
+		{uuid: '1', mailbox: 'Inbox', sender: 'Arfgnder Pierce', title: 'AdminLTE 2.0 Issue', date: 1432737154000, read: true, starred: true, attachment: false},
+		{uuid: '2', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: true, starred: true, attachment: false},
+		{uuid: '3', mailbox: 'Inbox', sender: 'Alexa Pierce', title: 'BdminLTE 2.0 Issue', date: 1431168277937, read: true, starred: true, attachment: false},
+		{uuid: '4', mailbox: 'Inbox', sender: 'Dany Sluijk', title: 'Mail test', date: 1430168277937, read: true, starred: true, attachment: false},
+		{uuid: '5', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: true, starred: true, attachment: false},
+		{uuid: '6', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLsfdshbcjbdvsbffvsgjfvsdgjfvsnvdsnbdsvvhdsvvhdsvdshvnbTE 2.0 Issue', read: false, date: 1433168277937, starred: true, attachment: false},
+		{uuid: '7', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: false, attachment: false},
+		{uuid: '8', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: false, attachment: true},
+		{uuid: '9', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: true, starred: true, attachment: true},
+		{uuid: '10', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: true, attachment: false},
+		{uuid: '11', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: false, attachment: true},
+		{uuid: '12', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: true, starred: false, attachment: true},
+		{uuid: '13', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: true, attachment: false},
+		{uuid: '14', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: true, starred: false, attachment: false},
+		{uuid: '15', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: true, attachment: true},
+		{uuid: '16', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: true, starred: true, attachment: false},
+		{uuid: '17', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: false, attachment: false},
+		{uuid: '18', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: false, attachment: true},
+		{uuid: '19', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: true, starred: true, attachment: false},
+		{uuid: '20', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: false, attachment: false},
+		{uuid: '21', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: true, starred: true, attachment: false},
+		{uuid: '22', mailbox: 'Inbox', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: true, attachment: false},
+	];
+	
+	//mail handling
     socket.on('mail:star', function(data){
-        for(var i in $scope.mails) {
-            if($scope.mails[i].uuid == data.uuid) {
-                $scope.mails[i].starred = data.state;
+        for(var i in $rootScope.mails) {
+            if($rootScope.mails[i].uuid == data.uuid) {
+                $rootScope.mails[i].starred = data.state;
                 break;
             }
         }
     });
     socket.on('mail:read', function(data){
-        for(var i in $scope.mails) {
-            if($scope.mails[i].uuid == data.uuid) {
-                $scope.mails[i].read = data.state;
+        for(var i in $rootScope.mails) {
+            if($rootScope.mails[i].uuid == data.uuid) {
+                $rootScope.mails[i].read = data.state;
                 break;
             }
         }
     });
     socket.on('mail:delete', function(data){
-        for(var i in $scope.mails) {
-            if($scope.mails[i].uuid == data.uuid) {
-                delete $scope.mails[i];
+        for(var i in $rootScope.mails) {
+            if($rootScope.mails[i].uuid == data.uuid) {
+                delete $rootScope.mails[i];
                 break;
             }
         }
     });
-    socket.emit('mail:delete', {uuid: '6'});
+});
+
+app.controller('mailboxController', function($rootScope, $scope, socket) {
+	$scope.title = 'Mailbox -> ';
+	$scope.subtitle = '';
+	$scope.mailbox = 'Inbox';
+	$scope.page = 1;
+	$scope.limit = 15;
+	$scope.mailCount = 0;
+    $scope.toggleStar = function star(uuid) {
+        for(var i in $rootScope.mails) {
+            if($rootScope.mails[i].uuid == uuid) {
+                socket.emit('mail:star', {uuid: uuid, state: !$rootScope.mails[i].starred});
+                break;
+            }
+        }
+		return true;
+    }
 	$scope.selected = [];
 	$scope.select = function select(uuid) {
-		console.log('test');
 		for(var i in $scope.selected) {
             if($scope.selected[i] == uuid) {
 				delete $scope.selected[i];
@@ -87,29 +115,32 @@ app.controller('mailboxController', function($scope, socket) {
             }
         }
 		$scope.selected.push(uuid);
+		return true;
 	}
-	$scope.mails = [
-		{uuid: '1', sender: 'Arfgnder Pierce', title: 'AdminLTE 2.0 Issue', date: 1432737154000, read: true, starred: true, attachment: false},
-		{uuid: '2', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: true, starred: true, attachment: false},
-		{uuid: '3', sender: 'Alexa Pierce', title: 'BdminLTE 2.0 Issue', date: 1431168277937, read: true, starred: true, attachment: false},
-		{uuid: '4', sender: 'Dany Sluijk', title: 'Mail test', date: 1430168277937, read: true, starred: true, attachment: false},
-		{uuid: '5', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: true, starred: true, attachment: false},
-		{uuid: '6', sender: 'Alexander Pierce', title: 'AdminLsfdshbcjbdvsbffvsgjfvsdgjfvsnvdsnbdsvvhdsvvhdsvdshvnbTE 2.0 Issue', read: false, date: 1433168277937, starred: true, attachment: false},
-		{uuid: '7', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: false, attachment: false},
-		{uuid: '8', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: false, attachment: true},
-		{uuid: '9', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: true, starred: true, attachment: true},
-		{uuid: '10', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: true, attachment: false},
-		{uuid: '11', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: false, attachment: true},
-		{uuid: '12', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: true, starred: false, attachment: true},
-		{uuid: '13', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: true, attachment: false},
-		{uuid: '14', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: true, starred: false, attachment: false},
-		{uuid: '15', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: true, attachment: true},
-		{uuid: '16', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: true, starred: true, attachment: false},
-		{uuid: '17', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: false, attachment: false},
-		{uuid: '18', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: false, attachment: true},
-		{uuid: '19', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: true, starred: true, attachment: false},
-		{uuid: '20', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: false, attachment: false},
-		{uuid: '21', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: true, starred: true, attachment: false},
-		{uuid: '22', sender: 'Alexander Pierce', title: 'AdminLTE 2.0 Issue', date: 1433168277937, read: false, starred: true, attachment: false},
-	];
+	$scope.deleteSelected = function deleteSelected() {
+		for(var i in $scope.selected) {
+			socket.emit('mail:delete', {uuid: $scope.selected[i]});
+			delete $scope.selected[i];
+		}
+		return true;
+	}
+	$scope.countMails = function countMails() {
+		$scope.mailCount = 0;
+		for(var i in $rootScope.mails) {
+			if($rootScope.mails[i].mailbox == $scope.mailbox) {
+				$scope.mailCount++;
+			}
+		}
+	}
+	$scope.selectAll = function selectAll() {
+		for(var i in $rootScope.mails) {
+			if($rootScope.mails[i].mailbox == $scope.mailbox) {
+				$scope.selected.push($rootScope.mails[i].uuid);
+			}
+		}
+	}
+	$scope.countMails();
+    socket.on('mail:delete', function(data){
+        $scope.countMails();
+    });
 });
