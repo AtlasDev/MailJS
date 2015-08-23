b34d31c121cb0efbd0dbce7af257dc4491284c81
AtlasDev <danysluyk@live.nl>
2015-08-23 22:27:45 +0200

    started system documentation, no code yet

---
+++ sys/client.js
@@ -0,0 +1,19 @@
+/**
+ * Create a new user.
+ * @name createUser
+ * @since 0.1.0
+ * @version 1
+ * @param {string} username Username to use.
+ * @param {string} password Password to use.
+ * @param {createUserCallback} callback Callback function after creating the user.
+ */
+
+/**
+ * Callback for creating a new user.
+ * @callback createUserCallback
+ * @param {Error} err Error object, should be undefined.
+ * @param {Object} user User object of the new created user.
+ */
+exports.create = function () {
+    //do stuff
+}
---
+++ sys/main.js
@@ -0,0 +1,1 @@
+//Exports all system functions in one file. You can require this file to access all functions.
