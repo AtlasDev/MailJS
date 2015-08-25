32cfecce6825f9f81321bd219d47892a9c039bee
AtlasDev <dany@atlasdev.nl>
2015-08-25 15:47:48 +0200

    added create user function

--- sys/user.js
+++ sys/user.js
@@ -1,3 +1,7 @@
+var util = require('../util.js');
+var User = require('../models/user.js');
+var Mailbox = require('../models/mailbox.js');
+
 /**
  * Create a new user.
  * @name createUser
@@ -5,6 +9,7 @@
  * @version 1
  * @param {string} username Username to use.
  * @param {string} password Password to use.
+ * @param {string} mailboxID Initial mailbox ID to assign the user to.
  * @param {createUserCallback} callback Callback function after creating the user.
  */
 
@@ -14,6 +19,27 @@
  * @param {Error} err Error object, should be undefined.
  * @param {Object} user User object of the new created user.
  */
-exports.create = function () {
-    //do stuff
+exports.create = function (username, password, mailboxID, callback) {
+    Mailbox.findById(mailboxID, function (err, mailbox) {
+        if(err) {
+            return callback(err, null);
+        }
+        if(!mailbox) {
+            var error = new Error('Mailbox ID not found.');
+            error.name = 'ENOTFOUND';
+            return callback(error, null);
+        }
+        var user = new User({
+            username: username,
+            password: password,
+            mailbox: [ mailboxID ],
+            group: 1
+        });
+        user.save(function(err) {
+            if (err) {
+                return callback(err, null);
+            }
+            return callback(null, user);
+        });
+    });
 }
