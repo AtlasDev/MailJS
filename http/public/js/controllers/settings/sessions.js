1a997da80ba2310e868c3b9dcdeef19bf1848dbd
AtlasDev <danysluyk@live.nl>
2015-09-24 08:29:05 +0200

    started parsing user agents

--- http/public/js/controllers/settings/sessions.js
+++ http/public/js/controllers/settings/sessions.js
@@ -13,7 +13,6 @@ app.controller("sessionsSettingsCtrl", function($rootScope, $scope, $http, user)
         };
         $http(req).then(function(res) {
             $scope.sessions = res.data.session;
-            console.log(res.data.session);
             $rootScope.isLoading = false;
         }, function(res) {
             notification.send('Internal Server Error', 'The server errored, please report this to your sysadmin.', 'error');
@@ -23,5 +22,31 @@ app.controller("sessionsSettingsCtrl", function($rootScope, $scope, $http, user)
     fetchSessions();
     $scope.reloadSessions = function () {
         fetchSessions();
+    }
+    $scope.getOS = function (ua) {
+        var os = new UAParser().setUA(ua).getResult().os;
+        console.log(os);
+    }
+    $scope.getBrowser = function (ua) {
+        var browser = new UAParser().setUA(ua).getResult().browser;
+        switch (browser.name) {
+            case 'Chrome':
+                return 'chrome';
+                break;
+            case 'Chromium':
+                return 'chrome';
+                break;
+            case 'Android Browser':
+                return 'android';
+                break;
+            case 'Firefox':
+                return 'firefox';
+                break;
+            case 'Vivaldi':
+                return 'vivaldi';
+                break;
+            default:
+                return 'unknown';
+        }
     }
 });
