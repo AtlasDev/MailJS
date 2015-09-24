59e83592aefaea27cc0298ebc779563a8fe3224a
AtlasDev <danysluyk@live.nl>
2015-09-24 15:53:52 +0200

    added OS images to sessions

--- http/public/js/controllers/settings/sessions.js
+++ http/public/js/controllers/settings/sessions.js
@@ -25,6 +25,43 @@ app.controller("sessionsSettingsCtrl", function($rootScope, $scope, $http, user)
     }
     $scope.getOS = function (ua) {
         var os = new UAParser().setUA(ua).getResult().os;
+        switch (os.name) {
+            case 'Windows':
+                switch (os.version) {
+                    case '7':
+                        return 'win7';
+                        break;
+                    case ('10' | '8'):
+                        return 'win10';
+                        break;
+                    default:
+                        return 'win';
+                }
+                break;
+            case 'Debian':
+                return 'debian';
+                break;
+            case 'Android':
+                return 'android';
+                break;
+            case 'Ubuntu':
+                return 'ubuntu';
+                break;
+            case 'Linux':
+                return 'linux';
+                break;
+            case 'iOS':
+                return 'ios';
+                break;
+            case 'Mac OS':
+                return 'ios';
+                break;
+            case ('Fedora' || 'Arch' || 'CentOS' || 'Gentoo' || 'Mint'):
+                return 'linux';
+                break;
+            default:
+                return 'unknown';
+        }
     }
     $scope.getBrowser = function (ua) {
         var browser = new UAParser().setUA(ua).getResult().browser;
@@ -43,6 +80,12 @@ app.controller("sessionsSettingsCtrl", function($rootScope, $scope, $http, user)
                 break;
             case 'Vivaldi':
                 return 'vivaldi';
+                break;
+            case 'Edge':
+                return 'edge';
+                break;
+            case 'IE':
+                return 'ie';
                 break;
             default:
                 return 'unknown';
