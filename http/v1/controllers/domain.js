03d55aa014c1d58acbd2279a25d2092c585ad6cc
AtlasDev <danysluyk@live.nl>
2015-09-24 21:11:56 +0200

    removed unnessessary log

--- http/v1/controllers/domain.js
+++ http/v1/controllers/domain.js
@@ -1,10 +1,10 @@
 var sys = require('../../../sys/main.js');
 
 exports.getDomains = function (req, res) {
-    console.log(sys.domain.getDomains(function (err, domains) {
+    sys.domain.getDomains(function (err, domains) {
         if(err) {
             return res.status(500).json({error: {name: err.name, message: err.message}});
         }
         return res.json({domains: domains});
-    }));
+    });
 }
