09df0d0a9f1d67fdea94f6b00559611db0c86184
AtlasDev <danysluyk@live.nl>
2015-12-02 23:50:35 +0100

    Concated the angulajs files together, not working yet

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+/*jslint node: true */
+"use strict";
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            dist: {
+                files: {
+                    'http/public/dist/app.js': ['http/public/dist/app.js']
+                },
+                options: {
+                    banner: '/* <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("dd-mm-yyyy") %> © AtlasDev*/',
+                    mangle: false
+                }
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['http/public/js/*.js', 'http/public/js/**/*.js'],
+                dest: 'http/public/dist/app.js'
+            }
+        },
+        watch: {
+            dev: {
+                files: ['http/public/js/*.js', 'http/public/js/**/*.js'],
+                tasks: ['jshint', 'concat:dist'],
+                options: {
+                    atBegin: true
+                }
+            },
+            min: {
+                files: ['http/public/js/*.js', 'http/public/js/**/*.js'],
+                tasks: ['jshint', 'concat:dist', 'uglify:dist'],
+                options: {
+                    atBegin: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('dev', ['watch:dev']);
+    grunt.registerTask('minified', ['watch:min']);
+    grunt.registerTask('package', ['concat:dist', 'uglify:dist']);
+};
