dd3456cbffd40f420c02402a57204eb37beaa41a
AtlasDev <dany@atlasdev.nl>
2015-12-03 12:20:29 +0100

    automated css minification

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,8 +32,26 @@ module.exports = function (grunt) {
                 dest: 'http/public/dist/app.min.js'
             }
         },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'http/public/dist/style.css': [
+                        'http/public/css/angular-toastr.min.css',
+                        'http/public/css/preloader.css',
+                        'http/public/css/style.css',
+                        'http/public/css/loader.css',
+                        'http/public/css/skins/skin-red-light.min.css',
+                        'http/public/css/bootstrap3-wysihtml5.min.css'
+                    ]
+                }
+            }
+        },
         watch: {
-            dev: {
+            devjs: {
                 files: [
                     'http/public/js/app.js',
                     'http/public/js/angular/*.js',
@@ -41,14 +59,14 @@ module.exports = function (grunt) {
                     'http/public/js/controllers/settings/*.js',
                     'http/public/js/factorys/*.js',
                     'http/public/js/other/*.js',
-                    '!http/public/js/other/js.cookie.js'
+                    '!http/public/js/other/js.cookie.js',
                 ],
                 tasks: ['concat:dist'],
                 options: {
                     atBegin: true
                 }
             },
-            min: {
+            minjs: {
                 files: [
                     'http/public/js/app.js',
                     'http/public/js/angular/*.js',
@@ -62,6 +80,20 @@ module.exports = function (grunt) {
                 options: {
                     atBegin: true
                 }
+            },
+            css: {
+                files: [
+                    'http/public/css/angular-toastr.min.css',
+                    'http/public/css/preloader.css',
+                    'http/public/css/style.css',
+                    'http/public/css/loader.css',
+                    'http/public/css/skins/skin-red-light.min.css',
+                    'http/public/css/bootstrap3-wysihtml5.min.css'
+                ],
+                tasks: ['cssmin'],
+                options: {
+                    atBegin: true
+                }
             }
         }
     });
@@ -69,9 +101,10 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    grunt.registerTask('dev', ['watch:dev']);
-    grunt.registerTask('minified', ['watch:min']);
-    grunt.registerTask('package', ['concat:dist', 'uglify:dist']);
-    grunt.registerTask('package-dev', ['concat:dist']);
+    grunt.registerTask('dev', ['watch:devjs', 'watch:css']);
+    grunt.registerTask('minified', ['watch:minjs', 'watch:css']);
+    grunt.registerTask('package', ['cssmin', 'concat:dist', 'uglify:dist']);
+    grunt.registerTask('package-dev', ['cssmin', 'concat:dist']);
 };
