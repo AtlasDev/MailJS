5ac0093002ad3f6f236548d2d57cead3fe1291ab
AtlasDev <danysluyk@live.nl>
2015-12-06 23:40:43 +0100

    added get email sys function and docs in prep for the GET email endpoint

--- models/email.js
+++ models/email.js
@@ -4,12 +4,13 @@
 var mongoose = require('mongoose');
 
 var EmailSchema  = new mongoose.Schema({
-    mailbox: { type: mongoose.Schema.Types.ObjectId, required: true },
+    inbox: { type: mongoose.Schema.Types.ObjectId, required: true },
     creationDate: { type: Number, required: true },
     reportedDate: { type: Number },
     sender: { type: String, required: true },
     senderDisplay: { type: String },
     content: { type: String, required: true },
+    preview: { type: String, required: true },
     subject: { type: String, required: true },
 });
 
--- sys/email.js
+++ sys/email.js
@@ -3,6 +3,7 @@
 
 var Email = require('../models/email.js');
 var validator = require('validator');
+var inbox = require('./inbox.js');
 
 /**
  * Create a new email
@@ -10,7 +11,7 @@ var validator = require('validator');
  * @since 0.1.0
  * @version 1
  * @param {String} mailboxID ID of the mailbox to add the email to.
- * @param {string} sender Envelope of the sender of the email.
+ * @param {String} sender Envelope of the sender of the email.
  * @param {String} subject The subject of the email.
  * @param {String} content The content of the email.
  * @param {createEmailCallback} callback Callback function after creating a new email.
@@ -52,16 +53,62 @@ exports.create = function (mailboxID, mail, cb) {
     var email = new Email();
     email.mailbox = mailboxID;
     email.creationDate = Math.round((new Date()).getTime() / 1000);
-    email.reportedDate = Math.round(mail.receivedDate.getTime() / 1000);
+    email.reportedDate = Math.round(mail.receivedDate.getTime() / 1000) || Math.round((new Date()).getTime() / 1000);
     email.sender = mail.from[0].address;
     email.senderDisplay = mail.from[0].name;
     email.subject = mail.subject;
     email.content = content;
+    email.preview = mail.text.substr(0, 100);
     email.save(function (err) {
         if(err) {
             return cb(err);
         }
         return cb(null, email);
+    });
+};
+
+/**
+ * Get emails from a inbox
+ * @name getEmails
+ * @since 0.1.0
+ * @version 1
+ * @param {String} mailboxID The id of the mailbox to get the emails from.
+ * @param {Number} limit Amount to limit the to gain emails.
+ * @param {String} skip Skip an amount of emails before returning.
+ * @param {getEmailsCallback} callback Callback function after getting the emails.
+ */
+
+/**
+ * Callback for creating a new email.
+ * @callback getEmailsCallback
+ * @param {Error} err Error object, should be undefined or passed trough.
+ * @param {Object} emails Preview of the found emails.
+ */
+exports.getEmails = function (inboxID, limit, skip, cb) {
+    var error;
+    if (!validator.isMongoId(mailboxID)) {
+        error = new Error('Invalid mailbox ID!');
+        error.name = 'EVALIDATION';
+        error.type = 400;
+        return cb(error);
+    }
+    if (typeof limit == "number" || limit > 100) {
+        error = new Error('Invalid limit!');
+        error.name = 'EVALIDATION';
+        error.type = 400;
+        return cb(error);
+    }
+    if (typeof skip == "number") {
+        error = new Error('Invalid skip!');
+        error.name = 'EVALIDATION';
+        error.type = 400;
+        return cb(error);
+    }
+    Email.find({inbox: inboxID}).sort('reportedDate', 1).skip(skip).limit(limit).exclude('content').exec(function (err, emails) {
+        if(err) {
+            return cb(err);
+        }
+        return cb(null, emails);
     });
 };
 }());
