974082ea04a6aca9d44332e7ac501090c5a0b5ce
AtlasDev <dany@atlasdev.nl>
2016-01-17 19:58:10 +0100

    display attachments to the user

--- http/public/js/controllers/mail.js
+++ http/public/js/controllers/mail.js
@@ -42,6 +42,41 @@ app.controller('mailCtrl', function($rootScope, $scope, $routeParams, $window, u
 		});
 	};
 
+	$scope.getAttachmentType = function (contentType) {
+		switch (contentType.split('/')[0]) {
+			case 'image':
+				return 'fa-file-image-o';
+			case 'audio':
+				return 'fa-file-audio-o';
+			case 'video':
+				return 'fa-file-video-o';
+			case 'application':
+				switch (contentType.split('/')[1]) {
+					case 'pdf':
+						return 'fa-file-pdf-o';
+					case 'octet-stream':
+						return 'fa-file-archive-o';
+					case 'zip':
+						return 'fa-file-archive-o';
+					case 'msword':
+						return 'fa-file-word-o';
+					case 'vnd.openxmlformats-officedocument.wordprocessingml.document':
+						return 'fa-file-word-o';
+					case 'vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+						return 'fa-file-exel-o';
+					case 'vnd.ms-powerpoint':
+						return 'fa-file-powerpoint-o';
+					case 'vnd.openxmlformats-officedocument.presentationml.presentation':
+						return 'fa-file-powerpoint-o';
+					default:
+						return 'fa-file-o';
+				}
+				break;
+			default:
+				return 'fa-file-o';
+		}
+	};
+
     if(typeof user.getUser()._id == 'undefined') {
         $rootScope.$on('userLoaded', function () {
             init();
