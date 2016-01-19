cf91aa4a1aeb805492c6b74345a7f3e21a2d903c
AtlasDev <dany@atlasdev.nl>
2016-01-19 10:01:00 +0100

    fixed errors

--- http/v1/controllers/invitations.js
+++ http/v1/controllers/invitations.js
@@ -4,12 +4,12 @@
 var sys = require('../../../sys/main.js');
 
 exports.create = function (req, res) {
-    if(req.user.isAdmin != true) {
+    if(req.user.isAdmin !== true) {
         return res.status(401).json({error: {name: 'EPERM', message: 'Permission denied'}});
     }
     sys.invitation.create(req.body.message, function (err, invite) {
         if (err) return res.status(err.type || 500).json({error: {name: err.name, message: err.message}});
         return res.json({invite: invite});
     });
-}
+};
 }());
